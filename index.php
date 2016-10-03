75618038f5feb20c27d3c4a90b2ab2d17332808c
ihewro <ihewro@163.com>
2016-10-03 11:43:30 +0800

    fixed about-page bug

--- index.php
+++ index.php
@@ -61,6 +61,81 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
   </div>
 </div>
+<!--首页大图动态效果-->
+<script src='<?php $this->options->themeUrl('assets/js/jquery.parallax.js'); ?>'></script>
+
+<script>
+
+var Leaf ={
+ F: function(id, w, h) {
+        var _height = $(id).parent().height(),
+            _width = $(id).parent().width(),
+            ratio = h / w;
+
+        if (_height / _width > ratio) {
+            id.style.height = _height +'px';
+            id.style.width = _height / ratio +'px';
+        } else {
+            id.style.width = _width +'px';
+            id.style.height = _width * ratio +'px';
+        }
+
+        id.style.left = (_width - parseInt(id.style.width)) / 2 +'px';
+        id.style.top = (_height - parseInt(id.style.height)) / 2 +'px';
+    }
+}
+var imageeffct = function() {
+
+
+    var cover = {};
+    cover.t = $('#cover');
+    cover.w = cover.t.attr('width');
+    cover.h = cover.t.attr('height');
+
+    ; (cover.o = function() {
+        $('#mark').height(window.innerHeight)
+    })();
+
+    ; (cover.f = function() {
+
+        var _w = $('#mark').width(),
+        _h = $('#mark').height(),
+        x,
+        y,
+        i,
+        e;
+
+        e = (_w >= 1000 || _h >= 1000) ? 1000 : 500;
+
+        if (_w >= _h) {
+            i = _w / e * 50;
+            y = i;
+            x = i * _w / _h;
+        } else {
+            i = _h / e * 50;
+            x = i;
+            y = i * _h / _w;
+        }
+
+        $('.layer').css({
+            'width': _w + x,
+            'height': _h + y,
+            'marginLeft': -0.5 * x,
+            'marginTop': -0.5 * y
+        })
+
+        if (!cover.w) {
+            cover.w = cover.t.width();
+            cover.h = cover.t.height();
+        }
+                Leaf.F($('#cover')[0], cover.w, cover.h);
+
+    })();
+
+    $('#mark').parallax();
+}
+imageeffct();
+</script>
 </div>
 
 <!-- menu -->
--- page.php
+++ page.php
@@ -1,21 +1,9 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 
+<div id="pjax-container">
 <div class="footer-clear post chinese">
 
-<div id="pjax-container">
-<style>
-#screen{
-	display:none;
-}
-</style>
-<div id="screen">
-	<div id="mark">
- 		<div class="layer" data-depth="0.20">
-               <img id="cover" src="http://isujin.com/wp-content/uploads/2016/04/wallhaven-212380.jpg" />
-		</div>
-	</div>
-</div>
   <!-- post header -->
   <div class="post-header">
     <h1 class="post-title"><?php $this->title() ?></h1>
