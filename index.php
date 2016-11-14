a748e652e6a19f51a894e8877502d1c95043a263
ihewro <ihewro@163.com>
2016-11-14 15:54:52 +0800

    优化首页体验

--- index.php
+++ index.php
@@ -116,86 +116,6 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  	background-color: #fff;
  }
 </style>
-<div class="index">
-<div id="zhezhao">
-</div>
-<?php if (!empty($this->options->indexsetup) && in_array('IndexImage', $this->options->indexsetup)): ?>
-<div id="screen">
-	<div id="mark">
- 		<div class="layer" data-depth="<?php $this->options->parallaxeffect();?>">
-<?php if (!empty($this->options->indexsetup) && in_array('Indexrandombackground', $this->options->indexsetup)): ?>
-               <img id="cover" src="<?php echo randombackground($this); ?>" />
-<?php else: ?>
-               <img id="cover" src="<?php $this->options->indeximage(); ?>" /> 
-<?php endif ?>
-		</div>
-	</div>
-</div>
-<?php else : ?>
-<div id="cover" style="background-image:url(<?php echo randombackground($this); ?>)"></div>
-<?php endif ;?>
-
-<?php if ( $this->options->Styleswitch =='0' ) : ?>
- <div id="style0">
-  <div class="vertical-center-child index-container">
-    <h1 class="index-title"><?php $this->options->IndexName(); ?></h1>
-    <div class="index-contacts">
-		<?php if ($this->options->socialgithub): ?>
-			<a class="social github" target="blank" href="<?php $this->options->socialgithub(); ?>">
-				<i class="fa fa-github-alt"></i>
-			</a>
-		<?php endif; ?>
-		<?php if ($this->options->socialmail): ?>
-			<a class="social envelope" href="<?php $this->options->socialmail(); ?>">
-				<i class="fa fa-envelope"></i>
-			</a>
-		<?php endif; ?>
-			<a class="social rss" target="blank" href="<?php $this->options->siteUrl(); ?>feed/">
-				<i class="fa fa-rss"></i>
-			</a>
-		<?php if ($this->options->socialbook): ?>
-			<a class="social book" target="blank" href="<?php $this->options->socialbook(); ?>">
-				<i class="fa fa-book"></i>
-			</a>
-		<?php endif; ?>
-			<a class="social search" id="search-Button" target="blank" href="<?php $this->options->socialweibo(); ?>">
-				<i class="fa fa-search"></i>
-			</a>
-            <div class="site-search col-3 kit-hidden-tb">
-                <form id="search" method="post" action="./" role="search">
-                    <label for="s" class="sr-only"><?php _e('搜索关键字'); ?></label>
-                    <input id="search-text" type="text" name="s" class="text" placeholder="<?php _e('输入关键字搜索'); ?>" />
-                    <button type="submit" id="search-submit" class="submit fa fa-search"></button>
-                </form>
-            </div>
-    </div>
-</div>
-  </div>
-<?php elseif($this->options->Styleswitch == '1'): ?>
-<div id="style1">
-  <div class="vertical-center-child index-container">
-    <h1 class="index-title"><?php $this->options->IndexName(); ?></h1>
-<div class="content2">
-<div class="p_part"><p><?php $this->options->IndexIntroduction(); ?></p>
-</div>
-</div>
-    <div class="index-contacts">
-<div class="buttons">
-<?php if ($this->options->socialmail): ?>
-<a href="<?php $this->options->socialmail(); ?>">My Email</a>
-<?php endif; ?>
-<?php if ($this->options->socialgithub): ?>
-<a href="<?php $this->options->socialgithub(); ?>" target="_blank" >My Github</a>
-<?php endif; ?>
-</div>
-    </div>
-</div>
-  </div>
-<?php endif; ?>
-
-
-
-</div>
 <script>
 <?php if (!empty($this->options->indexsetup) && in_array('IndexImage', $this->options->indexsetup)): ?>
 //首页大图动画效果
@@ -295,6 +215,86 @@ $(".index-title").click(function(){
 
 
 </script>
+<div class="index">
+<div id="zhezhao">
+</div>
+<?php if (!empty($this->options->indexsetup) && in_array('IndexImage', $this->options->indexsetup)): ?>
+<div id="screen">
+	<div id="mark">
+ 		<div class="layer" data-depth="<?php $this->options->parallaxeffect();?>">
+<?php if (!empty($this->options->indexsetup) && in_array('Indexrandombackground', $this->options->indexsetup)): ?>
+               <img id="cover" src="<?php echo randombackground($this); ?>" />
+<?php else: ?>
+               <img id="cover" src="<?php $this->options->indeximage(); ?>" /> 
+<?php endif ?>
+		</div>
+	</div>
+</div>
+<?php else : ?>
+<div id="cover" style="background-image:url(<?php echo randombackground($this); ?>)"></div>
+<?php endif ;?>
+
+<?php if ( $this->options->Styleswitch =='0' ) : ?>
+ <div id="style0">
+  <div class="vertical-center-child index-container">
+    <h1 class="index-title"><?php $this->options->IndexName(); ?></h1>
+    <div class="index-contacts">
+		<?php if ($this->options->socialgithub): ?>
+			<a class="social github" target="blank" href="<?php $this->options->socialgithub(); ?>">
+				<i class="fa fa-github-alt"></i>
+			</a>
+		<?php endif; ?>
+		<?php if ($this->options->socialmail): ?>
+			<a class="social envelope" href="<?php $this->options->socialmail(); ?>">
+				<i class="fa fa-envelope"></i>
+			</a>
+		<?php endif; ?>
+			<a class="social rss" target="blank" href="<?php $this->options->siteUrl(); ?>feed/">
+				<i class="fa fa-rss"></i>
+			</a>
+		<?php if ($this->options->socialbook): ?>
+			<a class="social book" target="blank" href="<?php $this->options->socialbook(); ?>">
+				<i class="fa fa-book"></i>
+			</a>
+		<?php endif; ?>
+			<a class="social search" id="search-Button" target="blank" href="<?php $this->options->socialweibo(); ?>">
+				<i class="fa fa-search"></i>
+			</a>
+            <div class="site-search col-3 kit-hidden-tb">
+                <form id="search" method="post" action="./" role="search">
+                    <label for="s" class="sr-only"><?php _e('搜索关键字'); ?></label>
+                    <input id="search-text" type="text" name="s" class="text" placeholder="<?php _e('输入关键字搜索'); ?>" />
+                    <button type="submit" id="search-submit" class="submit fa fa-search"></button>
+                </form>
+            </div>
+    </div>
+</div>
+  </div>
+<?php elseif($this->options->Styleswitch == '1'): ?>
+<div id="style1">
+  <div class="vertical-center-child index-container">
+    <h1 class="index-title"><?php $this->options->IndexName(); ?></h1>
+<div class="content2">
+<div class="p_part"><p><?php $this->options->IndexIntroduction(); ?></p>
+</div>
+</div>
+    <div class="index-contacts">
+<div class="buttons">
+<?php if ($this->options->socialmail): ?>
+<a href="<?php $this->options->socialmail(); ?>">My Email</a>
+<?php endif; ?>
+<?php if ($this->options->socialgithub): ?>
+<a href="<?php $this->options->socialgithub(); ?>" target="_blank" >My Github</a>
+<?php endif; ?>
+</div>
+    </div>
+</div>
+  </div>
+<?php endif; ?>
+
+
+
+</div>
 </div>
 <!-- menu -->
 <?php $this->need('menu.php'); ?>
