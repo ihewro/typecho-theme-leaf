82128fa3ac202f330b53c5b7b43e58031da7c0bf
ihewro <ihewro@163.com>
2016-11-17 14:22:57 +0800

    把多说表情图片全部使用相对路径本地化

--- assets/js/develope/embed.js
+++ assets/js/develope/embed.js
@@ -247,14 +247,14 @@ function(e, t, s) {
         y = [],
         _ = u = function() {
             function e(e) {
-                return t[e] || "&amp;"
+                return t[e] || "&"
             }
             var t = {
-                "<": "&lt;",
-                ">": "&gt;",
-                '"': "&quot;",
-                "'": "&#x27;",
-                "`": "&#x60;"
+                "<": "<",
+                ">": ">",
+                '"': """,
+                "'": "'",
+                "`": "`"
             },
             s = /&(?!\w+;)|[<>"'`]/g,
             a = /[&<>"'`]/;
@@ -979,15 +979,15 @@ if (window.devicePixelRatio != undefined && window.devicePixelRatio     >= 1.49)
     subfix = "@2x";
 }
 while(matched=s.message.match(/\#\s*<img[^\>]*?title=\"\[\s*(呵呵|哈哈|吐舌|太开心|笑眼|花心|小乖|乖|捂嘴笑|滑稽|你懂的|不高兴|怒|汗|黑线|泪|真棒|喷|惊哭|阴险|鄙视|酷|啊|狂汗|what|疑问|酸爽|呀咩爹|委屈|惊讶|睡觉|笑尿|挖鼻|吐|犀利|小红脸|懒得理|勉强|爱心|心碎|玫瑰|礼物|彩虹|太阳|星星月亮|钱币|茶杯|蛋糕|大拇指|胜利|haha|OK|沙发|手纸|香蕉|便便|药丸|红领巾|蜡烛|音乐|灯泡|开心|钱|咦|呼|冷|生气|弱|吐血)\s*\]\"[^\>]*?class=\"ds\-smiley\"[^\>]*?>/)) {
-    s.message = s.message.replace(matched[0], "<img src=\"http://www.ihewro.com/duoshuo/newpaopao/" + matched[1].trim() + subfix + ".png\" class=\"biaoqing paopao\" height=30 width=30 no-zoom />");
+    s.message = s.message.replace(matched[0], "<img src=\"/usr/themes/Leaf/duoshuo/newpaopao/" + matched[1].trim() + subfix + ".png\" class=\"biaoqing paopao\" height=30 width=30 no-zoom />");
 }
 
 while(matched=s.message.match(/\#\[\s*(呵呵|哈哈|吐舌|太开心|笑眼|花心|小乖|乖|捂嘴笑|滑稽|你懂的|不高兴|怒|汗|黑线|泪|真棒|喷|惊哭|阴险|鄙视|酷|啊|狂汗|what|疑问|酸爽|呀咩爹|委屈|惊讶|睡觉|笑尿|挖鼻|吐|犀利|小红脸|懒得理|勉强|爱心|心碎|玫瑰|礼物|彩虹|太阳|星星月亮|钱币|茶杯|蛋糕|大拇指|胜利|haha|OK|沙发|手纸|香蕉|便便|药丸|红领巾|蜡烛|音乐|灯泡|开心|钱|咦|呼|冷|生气|弱|吐血)\s*\]/)) {
-    s.message = s.message.replace(matched[0], "<img src=\"http://www.ihewro.com/duoshuo/newpaopao/" + matched[1] + subfix + ".png\" class=\"biaoqing paopao\" height=30 width=30 no-zoom />");
+    s.message = s.message.replace(matched[0], "<img src=\"/usr/themes/Leaf/duoshuo/newpaopao/" + matched[1] + subfix + ".png\" class=\"biaoqing paopao\" height=30 width=30 no-zoom />");
 }
 
             while (matched = s.message.match(/\#\(\s*(高兴|小怒|脸红|内伤|装大款|赞一个|害羞|汗|吐血倒地|深思|不高兴|无语|亲亲|口水|尴尬|中指|想一想|哭泣|便便|献花|皱眉|傻笑|狂汗|吐|喷水|看不见|鼓掌|阴暗|长草|献黄瓜|邪恶|期待|得意|吐舌|喷血|无所谓|观察|暗地观察|肿包|中枪|大囧|呲牙|抠鼻|不说话|咽气|欢呼|锁眉|蜡烛|坐等|击掌|惊喜|喜极而泣|抽烟|不出所料|愤怒|无奈|黑线|投降|看热闹|扇耳光|小眼睛|中刀)\s*\)/)) {
-                s.message = s.message.replace(matched[0], "<img src=\"http://www.ihewro.com/duoshuo/alu/" + matched[1] + subfix + ".png\" class=\"biaoqing alu\" height=33 width=33 no-zoom />")
+                s.message = s.message.replace(matched[0], "<img src=\"/usr/themes/Leaf/duoshuo/alu/" + matched[1] + subfix + ".png\" class=\"biaoqing alu\" height=33 width=33 no-zoom />")
             }
 
 
@@ -2039,7 +2039,7 @@ while(matched=s.message.match(/\#\[\s*(呵呵|哈哈|吐舌|太开心|笑眼|花
                             if (window.devicePixelRatio != undefined && window.devicePixelRatio >= 1.49) {
                                 subfix = "@2x"
                             }
-                            i = "http://www.ihewro.com/duoshuo/" + s + subfix + ".png"
+                            i = "/usr/themes/Leaf/duoshuo/" + s + subfix + ".png"
                         }
 
                         "WordPress" === e && (t = " " + t + " "),
